a01db097dded7c3a9daec925b9963233744e8730
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-09-18 21:10:44 -0300

    removing comments

--- src/js/index.js
+++ src/js/index.js
@@ -4,19 +4,10 @@ function countdown(totalSeconds) {
   const years = Math.floor(totalSeconds / 3600 / 24 / 30 / 12 - 1);
   const months = Math.floor((totalSeconds / 3600 / 24 / 30) % 12);
   const days = Math.floor((totalSeconds / (3600 * 24)) % 30);
-  // const hours =  Math.floor((totalSeconds / 3600) % 24);
-  // const minutes = Math.floor((totalSeconds / 60) % 60);
-  // const seconds = Math.floor(totalSeconds % 60);
-
-  // document.querySelector("#timerSeconds").innerHTML = formatTime(seconds);
-  // document.querySelector("#timerMinutes").innerHTML = formatTime(minutes);
-  // document.querySelector("#timerHours").innerHTML = formatTime(hours);
   document.querySelector("#timerDays").innerText = formatTime(days);
   document.querySelector("#timerMonths").innerText = formatTime(months);
   document.querySelector("#timerYears").innerText = formatTime(years);
 }
-
-// const timeIn = getTime();
 
 let time = {};
 
@@ -24,7 +15,6 @@ form.yearsTarget.addEventListener(
   "input",
   (e) => (time.lifeTimeYears = parseInt(e.target.value))
 );
-// form
 
 form.birthday.addEventListener("input", (e) => {
   const yearInMiliseconds = 31557600000;
@@ -47,7 +37,6 @@ form.birthday.addEventListener("input", (e) => {
 
   time.finalDate = CalcfinalDate();
 
-  // const totalSeconds = (time.timeLeftYears) * yearInMiliseconds / 1000;
   const totalSeconds = (time.finalDate - currentDate) / 1000;
   console.log(totalSeconds);
   console.log(time);
